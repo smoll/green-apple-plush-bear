4eefc17d28760fa75cea19bbaa8fe7b71a16e7b4
Shujon Mollah <mollah@gmail.com>
2018-02-20 13:56:21 -0500

    Calculate total

--- app.py
+++ app.py
@@ -2,10 +2,29 @@ from logzero import logger
 import redis
 import requests
 
+def _diff(array):
+    """
+    Returns integer diff between max and min integer in array.
+    If array contains any numerical anagrams, returns None.
+    """
+    def anagram_for(num):
+        return int(str(num, 'utf-8')[::-1])
+
+    checked = []
+    for item in array:
+        # logger.debug('item: %s' % (item,))
+        if anagram_for(item) in checked:
+            return None
+        checked.append(int(item))
+
+    return max(checked) - min(checked)
+
 logger.info('starting...')
 
 r = redis.StrictRedis(host='redis', port=6379)
 keys = r.keys('*')
+
+total = 0
 for key in keys:
     type = r.type(key)
     logger.info('type: %s' % type)
@@ -16,7 +35,13 @@ for key in keys:
         val = r.smembers(key)
     else:
         raise BaseException('FIXME: unhandled redis type!')
+    logger.info('val: %s' % (val,))
 
-    logger.info('val: %s' % val)
+    diff = _diff(val)
+    logger.info('diff: %s' % (diff,))
 
+    if diff:
+        total += diff
+
+logger.info('total: %s' % total)
 logger.info('done.')
